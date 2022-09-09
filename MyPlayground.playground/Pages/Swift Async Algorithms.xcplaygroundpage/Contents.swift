a70927e6d633e526903b559f313d1321a9c8d4a9
KokiHirokawa <kitakitsune1110@gmail.com>
2022-09-10 01:46:44 +0900

    Continue learning swift async algorithm

--- MyPlayground.playground/Pages/Swift Async Algorithms.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Swift Async Algorithms.xcplaygroundpage/Contents.swift
@@ -15,3 +15,18 @@ let stream2 = AsyncStream<String> { continuation in
 for await (num, str) in zip(stream, stream2) {
     print(num, str)
 }
+
+let stream3 = AsyncStream<Int> { continuation in
+    continuation.yield(4)
+    continuation.yield(5)
+    continuation.yield(6)
+}
+
+for await num in merge(stream, stream3) {
+    print(num)
+}
+
+let batches = stream.chunked(by: .repeating(every: .milliseconds(500)))
+for await nums in batches {
+    print(nums)
+}
